544ee1b2170ab09e6fe48dd9718f67446d78c50b
Sapodilla <jakebaxter8@hotmail.co.uk>
2023-07-23 11:16:23 +0100

    Added GliderPawn

---
+++ Source/VehicleGame/Private/GliderPawn.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VehicleGame/Public/GliderPawn.h"
+#include "Components/CapsuleComponent.h"
+
+
+// Sets default values
+AGliderPawn::AGliderPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	//FirstPersonCameraComponent->SetupAttachment(Root? Mesh?);
+	FirstPersonCameraComponent->SetRelativeLocation(FVector(-10.f, 0.f, 60.f)); // Position the camera
+	FirstPersonCameraComponent->bUsePawnControlRotation = false;
+
+}
+
+// Called when the game starts or when spawned
+void AGliderPawn::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (const APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(MappingContext, 0);
+		}
+	}
+}
+
+// Called every frame
+void AGliderPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AGliderPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MovementAction, ETriggerEvent::Triggered, this, &AGliderPawn::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AGliderPawn::Look);
+	}
+
+}
+
+void AGliderPawn::Move(const FInputActionValue& Value)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Movement"));
+}
+
+void AGliderPawn::Look(const FInputActionValue& Value)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Movement"));
+	
+}
---
+++ Source/VehicleGame/Public/GliderPawn.h
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/Pawn.h"
+
+//Input includes
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+#include "GliderPawn.generated.h"
+
+UCLASS()
+class VEHICLEGAME_API AGliderPawn : public APawn
+{
+	GENERATED_BODY()
+
+
+	//Movement of the camera is seperate to the main pawn.
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = true))
+	UCameraComponent* FirstPersonCameraComponent;
+
+
+#pragma region Input
+
+	UPROPERTY(EditDefaultsOnly, Category = Input)
+	UInputMappingContext* MappingContext;
+
+	UPROPERTY(EditDefaultsOnly, Category = Input)
+	UInputAction* MovementAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = Input)
+	UInputAction* LookAction;
+
+#pragma endregion Input
+
+
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+
+	
+
+public:
+	// Sets default values for this pawn's properties
+	AGliderPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
