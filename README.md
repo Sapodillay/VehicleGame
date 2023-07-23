2e86d33120d0ba813fad20f281c47c2c91fc50fd
Sapodilla <jakebaxter8@hotmail.co.uk>
2023-07-23 09:37:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# VehicleGame
+
+Developed with Unreal Engine 5
---
+++ Source/VehicleGame/VehicleGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VehicleGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VEHICLEGAME_API AVehicleGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
